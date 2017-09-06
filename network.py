45a8b0ad9d2446524f8cad0ceb1f501c0e0bd288
Maaz Zulfiqar <maazi.zulfi@gmail.com>
2017-09-06 17:36:50 +0200

    detection network for cifar images

---
+++ network.py
@@ -0,0 +1,31 @@
+from keras.models import Model,Sequential
+from keras.layers import Conv2D, MaxPool2D, Input, ZeroPadding2D, Dense, Flatten, Activation
+
+x1 = Input(batch_shape=(None, 32, 32, 3))
+
+x = ZeroPadding2D(padding=(1,1))(x1)# 34,34,3
+x = Conv2D(filters= 64,kernel_size=(3,3),strides=(1,1), activation='relu')(x)#32,32,64
+x = MaxPool2D(pool_size=(2, 2))(x)#16,16,64
+
+x = ZeroPadding2D(padding=(1,1))(x)# 18,18,64
+x = Conv2D(filters= 128,kernel_size=(3,3),strides=(1,1), activation='relu')(x)#16,16,64
+x = MaxPool2D(pool_size=(2, 2))(x)#8,8,64
+
+x = ZeroPadding2D(padding=(1,1))(x)# 10,10,3
+x = Conv2D(filters= 256,kernel_size=(3,3),strides=(1,1), activation='relu')(x)#8,8,64
+x = MaxPool2D(pool_size=(2, 2))(x)#4,4,64
+
+x = ZeroPadding2D(padding=(1,1))(x)# 6,6,3
+x = Conv2D(filters= 256,kernel_size=(3,3),strides=(1,1), activation='relu')(x)#4,4,64
+x = MaxPool2D(pool_size=(2, 2))(x)#2,2,256
+
+x = Flatten()(x) #1024
+x = Dense(units=1)(x)
+x = Activation(activation='hard_sigmoid')(x)
+
+model = Model(x1,x)
+model.compile(optimizer='adam',loss='mean_squared_error',metrics=['accuracy'])
+print model.summary()
+model.fit()
+#model = Sequential()
+#model.add(Conv2D())
