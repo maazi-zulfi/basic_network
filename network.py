555b7ae8a798eae477cdb558a2a5562f81a6dd5a
Maaz Zulfiqar <maazi.zulfi@gmail.com>
2017-09-06 22:38:26 +0200

    reversed color channel swap

--- network.py
+++ network.py
@@ -12,6 +12,8 @@ label = dict['labels']
 train_data = data/255.0
 train_data = np.reshape(train_data,(-1,3,32,32))
 train_data = np.transpose(train_data,[0,2,3,1])
+label = np.array(label)
+label = np.array([label==0,label==1,label==2,label==3,label==4,label==5,label==6,label==7,label==8,label==9],dtype=np.int)
 
 x1 = Input(batch_shape=(None, 32, 32, 3))
 
@@ -32,11 +34,11 @@ x = Conv2D(filters= 256,kernel_size=(3,3),strides=(1,1), activation='relu')(x)#4
 x = MaxPool2D(pool_size=(2, 2))(x)#2,2,256
 
 x = Flatten()(x) #1024
-x = Dense(units=1)(x)
-x = Activation(activation='sigmoid')(x)
+x = Dense(units=10)(x)
+x = Activation(activation='softmax')(x)
 
 model = Model(x1,x)
-model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
 print model.summary()
 print "maaz is stupid"
 model.fit(x=train_data,y=label,batch_size=32,epochs=50)
