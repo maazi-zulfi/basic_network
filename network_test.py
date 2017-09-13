2be60bf98ce481b655940680a651f225b462f7d3
Maaz Zulfiqar <maazi.zulfi@gmail.com>
2017-09-13 14:05:51 +0200

    Model load

--- network_test.py
+++ network_test.py
@@ -5,3 +5,4 @@ model = load_model('/home/maaz/dev/saved_models/basic_network.h5')
 
 #with open('/home/maaz/dev/projects/data/cifar-10-batches-py/test_batch','rb') as fo:
    # dict = cPickle.load(fo)
+print load_model('/home/maaz/dev/saved_models/basic_network.h5')
